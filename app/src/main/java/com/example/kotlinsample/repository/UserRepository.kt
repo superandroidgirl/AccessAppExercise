0b27b7a7af890993c462309a653227a7131d9ff1
iris <e60626@gmail.com>
2019-12-02 15:23:59 +0800

    remove useless code

--- app/src/main/java/com/example/kotlinsample/repository/UserRepository.kt
+++ app/src/main/java/com/example/kotlinsample/repository/UserRepository.kt
@@ -1,9 +1,6 @@
 package com.example.kotlinsample.repository
 
-import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
-import kotlinx.serialization.json.Json
 import okhttp3.logging.HttpLoggingInterceptor
-import okhttp3.MediaType
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
